6aa52d7d73cced5e2124a5359d4fd3c1845abae6
k6zma <87338187+k6zma@users.noreply.github.com>
2024-01-29 18:10:30 +0300

    pre-finished work on nude_classification

---
+++ src/face_AI/learning_models/nude_classifiyng.py
@@ -0,0 +1,150 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torchvision import datasets, models, transforms
+import time
+import os
+import copy
+from tqdm import tqdm
+
+def main():
+    device = torch.device("mps")
+
+    data_dir = 'data/nude_detection/nude_classification_images'
+
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.Resize(224),
+            transforms.CenterCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(60),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'val': transforms.Compose([
+            transforms.Resize(224),
+            transforms.CenterCrop(224),
+            transforms.RandomRotation(60),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'test': transforms.Compose([
+            transforms.Resize(224),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+    }
+
+    image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x])
+                    for x in tqdm(['train', 'val', 'test'])}
+    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=64, shuffle=True, num_workers=4)
+                for x in tqdm(['train', 'val', 'test'])}
+    dataset_sizes = {x: len(image_datasets[x]) for x in tqdm(['train', 'val', 'test'])}
+
+    class_names = image_datasets['train'].classes
+
+    model_ft = models.efficientnet_b2(weights=models.EfficientNet_B2_Weights.IMAGENET1K_V1)
+
+    for param in model_ft.parameters():
+        param.requires_grad = False
+
+    num_ftrs = model_ft.classifier[1].in_features
+    model_ft.classifier[1] = nn.Linear(num_ftrs, 2)
+
+    model_ft = model_ft.to(device)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer_ft = optim.Adam(model_ft.classifier.parameters(), lr=0.001)
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+
+    train_loss = []
+    val_loss = []
+    train_acc = []
+    val_acc = []
+    epoch_counter_train = []
+    epoch_counter_val = []
+
+    def train_model(model, criterion, optimizer, scheduler, num_epochs):
+        since = time.time()
+
+        best_model_wts = copy.deepcopy(model.state_dict())
+        best_acc = 0.0
+
+        for epoch in range(num_epochs):
+            print('Epoch {}/{}'.format(epoch + 1, num_epochs))
+            print('-' * 10)
+
+            for phase in ['train', 'val']:
+                if phase == 'train':
+                    scheduler.step()
+                    model.train()
+                else:
+                    model.eval()
+
+                running_loss = 0.0
+                running_corrects = 0
+
+                for inputs, labels in tqdm(dataloaders[phase]):
+                    inputs = inputs.to(device)
+                    labels = labels.to(device)
+
+                    optimizer.zero_grad()
+
+                    with torch.set_grad_enabled(phase == 'train'):
+                        outputs = model(inputs)
+                        _, preds = torch.max(outputs, 1)
+                        loss = criterion(outputs, labels)
+
+                        if phase == 'train':
+                            loss.backward()
+                            optimizer.step()
+                            scheduler.step()
+
+                    running_loss += loss.item() * inputs.size(0)
+                    running_corrects += torch.sum(preds == labels.data)
+
+                if phase == "train":
+                    train_loss.append(running_loss/dataset_sizes[phase])
+                    train_acc.append(running_corrects.float() /
+                                    dataset_sizes[phase])
+                    epoch_counter_train.append(epoch)
+                if phase == "val":
+                    val_loss.append(running_loss / dataset_sizes[phase])
+                    val_acc.append(running_corrects.float() /
+                                dataset_sizes[phase])
+                    epoch_counter_val.append(epoch)
+
+                epoch_loss = running_loss / dataset_sizes[phase]
+                epoch_acc = running_corrects.float() / dataset_sizes[phase]
+
+                if phase == "train":
+                    epoch_loss = running_loss / dataset_sizes[phase]
+                    epoch_acc = running_corrects.float() / dataset_sizes[phase]
+                if phase == "val":
+                    epoch_loss = running_loss / dataset_sizes[phase]
+                    epoch_acc = running_corrects.float() / dataset_sizes[phase]
+
+                print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                    phase, epoch_loss, epoch_acc))
+
+                if phase == 'val' and epoch_acc > best_acc:
+                    best_acc = epoch_acc
+                    best_model_wts = copy.deepcopy(model.state_dict())
+
+        time_elapsed = time.time() - since
+        print('Training complete in {:.0f}m {:.0f}s'.format(
+            time_elapsed // 60, time_elapsed % 60))
+        print('Best val Acc: {:4f}'.format(best_acc))
+
+        model.load_state_dict(best_model_wts)
+        return model
+
+
+    model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=25)
+
+    torch.save(model_ft.state_dict(), 'training_results/weights/nude_detection/model_efficientnet.pth')
+
+if __name__ == '__main__':
+    main()
